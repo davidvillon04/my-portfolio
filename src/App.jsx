d762cb9906528af716c0a90082d46833fb2e721b
davidvillon04 <davidv04@vt.edu>
2025-05-19 13:57:39 -0400

    Importing Routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+// Importing the routes
+import Home from "./routes/Home.jsx";
+import Projects from "./routes/Projects.jsx";
+import About from "./routes/About.jsx";
+
 function App() {
   return (
     <>
-      <p> Hello World! </p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     </>
   );
 }
