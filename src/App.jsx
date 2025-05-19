83d365b9d155071d6493bc06f8162193b7fb69bf
davidvillon04 <davidv04@vt.edu>
2025-05-19 14:53:51 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
 // Importing the routes
 import Home from "./routes/Home.jsx";
 import Projects from "./routes/Projects.jsx";
 import About from "./routes/About.jsx";
+import Contact from "./routes/Contact.jsx";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
+      <Box
+        sx={{
+          minHeight: "100vh",
+          backgroundColor: "rgba(0, 0, 0, 0.44)",
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Box>
     </>
   );
 }
